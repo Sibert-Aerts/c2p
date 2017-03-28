c19b2e04b6f50e986502cc4afef4920f178f351a
sibert-aerts <sibert.aerts@gmail.com>
2017-03-28 17:36:05 +0200

    add garbage.c

---
+++ src/examples/garbage.c
@@ -0,0 +1,42 @@
+// file full of bizarre grammatically correct scenarios
+
+void  *****const*const*const*** main(void flup[x][y + 2][-100], int x, const const float y, char (*blub)[]){
+
+    // empty
+    ;;;;;;;;;;
+    {}{}{}{}{}{}{}{}
+    {{{{{{{{{{{}}}}}}}}}}}
+    
+    // pointless
+    "fifteen";
+    15;
+    fifteen;
+    "the number", is, "fifteen";
+    15.0e15;
+    
+    
+    // repetitive
+    const const const int const const const;
+    char ******************************************************* pointer;
+    float (*const(*const(*const(*const(*const(*const(*const(*const x[10])[10])[10])[10])[10])[10])[10])[10])[10];
+    x + + + + + + + + + 1;
+    y - - - - - - - - - 1;
+    ----------------------------x;
+    y++++++++++++++++++++++++++++;
+    (((((((((((((((((((((((x)))))))))))))))))))))));
+    f(f(f(f(f(f(f(f())))))));
+    x[x[x[x[x[x[x[x[x[x]]]]]]]]];
+    (const int*const)(const int*const)(const int*const)(const int*const)x;
+    int(((((((((((((((((((((x)))))))))))))))))))));
+    
+    // convoluted
+    a ? b ? c ? d ? e ? f : g : h : i : j : k;
+    a = b = c = d = e = f;
+    a = b , c = d , e = f;
+    a+++b---c+++d---e+++f;
+    a* *b* *c* *d* *e* *f;
+    (a, b, c)   (d, e, f);
+     (a)(b)(c)(d)(e)(f);
+     (a(b(c(d(e(f))))));
+     
+}
