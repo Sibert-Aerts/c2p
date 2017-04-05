548b17edfb7ab8b1b2742ef6f30f3e81efab9dc9
sibert-aerts <sibert.aerts@gmail.com>
2017-04-05 17:12:31 +0200

    Move tests around

--- src/c2p/grammar/ast/test_visitor.py
+++ src/c2p/grammar/ast/test_visitor.py
@@ -10,40 +10,51 @@ from c2p.grammar.antlr.SmallCLexer import SmallCLexer
 from c2p.grammar.antlr.SmallCParser import SmallCParser
 from c2p.grammar.ast.visitor import ASTVisitor
 
-filepath = os.path.dirname(__file__) + "/../examples/"
+filepath = os.path.dirname(__file__) + "/test/"
 
 class TestVisitorSuccess(unittest.TestCase):
     def parse(self, filename):
         parser = SmallCParser(CommonTokenStream(SmallCLexer(FileStream(filename))))
         self.tree = parser.program()
 
-    def _test(self, filename):
-        self.parse(filepath + filename)
-        AST = ASTVisitor().visit(self.tree)
+    def _test_acceptance(self, filename):
+        try:
+            self.parse(filepath + filename)
+            ASTVisitor().visit(self.tree)
+        except:
+            self.fail('Failed to visit \'{0}\'!'.format(filename))
+
+    def _test_failure(self, filename):
+        try:
+            self.parse(filepath + filename)
+            ASTVisitor().visit(self.tree)
+        except:
+            return
+        self.fail('Succeeded at visiting incorrect file \'{0}\'!'.format(filename))
 
     def test_etc(self):
-        self._test('etc.c')
+        self._test_acceptance('visitable/etc.c')
 
     def test_for(self):
-        self._test('for.c')
+        self._test_acceptance('visitable/for.c')
 
     def test_if(self):
-        self._test('if.c')
+        self._test_acceptance('visitable/if.c')
 
     def test_while(self):
-        self._test('while.c')
+        self._test_acceptance('visitable/while.c')
 
     def test_function(self):
-        self._test('function.c')
+        self._test_acceptance('visitable/function.c')
 
     def test_expression(self):
-        self._test('expression.c')
+        self._test_acceptance('visitable/expression.c')
 
     def test_garbage(self):
-        self._test('garbage.c')
+        self._test_acceptance('visitable/garbage.c')
+
+    def test_qualifiers(self):
+        self._test_failure('unvisitable/qualifiers.c')
 
     def tearDown(self):
         self.tree = None
-
-if __name__ == '__main__':
-    unittest.main()
