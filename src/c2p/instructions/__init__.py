c4c7b84e1c323f1c3c18534356b4d984e660dd3c
sibert-aerts <sibert.aerts@gmail.com>
2017-04-09 17:28:30 +0200

    Fix instructions init

--- src/c2p/instructions/__init__.py
+++ src/c2p/instructions/__init__.py
@@ -1,11 +1,11 @@
 from .base import PInstruction
 
 from .arithmetic import Add, Sub, Mul, Div, Neg, Inc, Dec
-from .array import Ixa, Chk, Dpl, Ldd, Sli, Lod, Lda, Str
+from .array import Ixa, Chk, Dpl, Ldd, Sli
 from .branch import Ujp, Fjp, Ixj, Label
 from .comparison import Equ, Geq, Leq, Les, Grt, Neq
 from .io import In, Out1, Out2
 from .logic import And, Or, Not
 from .misc import New, Hlt, Conv, Movs, Movd
 from .procedure import Mst, Cup, Ssp, Sep, Ent, Retf, Retp, Smp, Cupi, Mstf
-from .store_load import Ldo, Ldc, Ind, Sro, Sto
+from .store_load import Ldo, Ldc, Ind, Sro, Sto, Lod, Lda, Str
